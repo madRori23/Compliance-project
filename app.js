cf0ccc9b676aab28cca57072364f34d0f77a89fd
Ororiseng Madibela <ororisengmadibela@gmail.com>
2026-02-14 16:58:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -719,7 +719,9 @@ viewUserDetails(userId) {
       <section class="stats-section">
         <div class="stats-grid">
           <div class="stat-card">
-            <span class="stat-icon">🧪</span>
+            <span class="stat-icon">
+             <img src="https://www.google.com/imgres?q=quality%20assurance%20mobile%20manual%20test%20icon&imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fimage-vector%2Fblack-single-approved-clipboard-icon-260nw-2609522621.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fmanual-testing-icon&docid=6oRsdb0H0GXqkM&tbnid=T4EaKtv2DO2DsM&vet=12ahUKEwjoiKKvntmSAxUVZ0EAHcQpLXYQnPAOegQIexAB..i&w=260&h=280&hcb=2&ved=2ahUKEwjoiKKvntmSAxUVZ0EAHcQpLXYQnPAOegQIexAB">
+            </span>
             <div class="stat-content">
               <h3>${testsToday}</h3>
               <p>Tests Today</p>
@@ -843,7 +845,9 @@ viewUserDetails(userId) {
           <div class="max-h-500 overflow-y-auto">
             ${userTests.length === 0 ? `
               <div class="empty-state">
-                <div class="empty-icon">🧪</div>
+                <div class="empty-icon">
+                  <img src="https://www.google.com/imgres?q=quality%20assurance%20mobile%20manual%20test%20icon&imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fimage-vector%2Fblack-single-approved-clipboard-icon-260nw-2609522621.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fmanual-testing-icon&docid=6oRsdb0H0GXqkM&tbnid=T4EaKtv2DO2DsM&vet=12ahUKEwjoiKKvntmSAxUVZ0EAHcQpLXYQnPAOegQIexAB..i&w=260&h=280&hcb=2&ved=2ahUKEwjoiKKvntmSAxUVZ0EAHcQpLXYQnPAOegQIexAB">
+                </div>
                 <p>No tests recorded yet</p>
               </div>
             ` : userTests.map(test => `
@@ -1911,6 +1915,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   await window.app.init();
 
 });
+
 
 
 
